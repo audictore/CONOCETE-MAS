0b66351f2232f276817bee6357a1de0f24822c2a
Alonzo Lara Flores <131033118+audictore@users.noreply.github.com>
2024-08-08 10:51:22 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,110 @@
+const form_registrer = document.getElementById("form_registrer")
+
+form_registrer.addEventListener("submit", validar_formulario)
+
+function validar_formulario(e){
+    e.preventDefault()
+
+    const name_user = document.getElementById("name_user").value
+    const sex_user = document.querySelector('input[name="sex"]:checked')
+    const age_user = document.getElementById("age_user").value
+    const juego_user = document.querySelectorAll('input[name="juego"]:checked')
+    const ciudad_user = document.getElementById("ciudad_user").value
+    const pelicula_user = document.getElementById("pelicula_user").value
+    const span_error = document.getElementById("span_error")
+
+    let valid = true;
+
+
+    //validar nombre
+    if(name_user.match(/[0-9]/)){
+        span_error.innerHTML = "El nombre no debe tener ningun numero."
+        valid = false
+    }else if(name_user.match(/[!@#$%^&*(),.?":{}|<>]/)){
+        span_error.innerHTML = "El nombre no debe tener ningun caracter especial."
+        valid = false
+    }else if(name_user == ""){
+        span_error.innerHTML = "Debes escribir algun nombre."
+        valid = false
+    }
+
+    //validar sexo
+    if(!sex_user){
+        span_error.innerHTML = "Debes seleccionar un sexo."
+        valid = false
+    }
+
+    //validar edad
+    if(age_user.match(/[a-z]/)){
+        span_error.innerHTML = "La edad no debe tener ninguna letra."
+        valid = false
+    }else if(age_user.match(/[A-Z]/)){
+        span_error.innerHTML = "La edad no debe tener ninguna letra."
+        valid = falseelse 
+    }else if(age_user.match(/[!@#$%^&*(),.?":{}|<>]/)){
+        span_error.innerHTML = "La edad no debe tener ningun caracter especial."
+        valid = false
+    }else if(age_user == ""){
+        span_error.innerHTML = "Debes escribir alguna edad."
+        valid = false
+    }else if(age_user < 0){
+        span_error.innerHTML = "Debes escribir una edad valida."
+        valid = false
+    }
+
+    //validar juegos
+    if(juego_user.length < 1) {
+        span_error.innerHTML = "Debes elegir un juego."
+        valid = false
+        
+    }else if(juego_user.length > 1){
+        span_error.innerHTML = "Debes elegir un juego."
+        valid = false
+    }
+    
+    //validar ciudad
+    if(ciudad_user === ""){
+        span_error.innerHTML = "Debes elegir una ciudad."
+        valid = false;
+    }
+
+    //validar pelicula
+    if(pelicula_user === ""){
+        span_error.innerHTML = "Debes elegir una pelicula."
+        valid = false;
+    }
+
+    //mensaje de salida
+    if(valid=true){
+        alert("Hola "+name_user+" tu sexo es: "+sex_user.value+". Tu edad es de: "+age_user+" años. Tu juego favorito es: "+juego_user[0].value+". Vives en: "+ciudad_user+". Tu pelicula favorita es: "+pelicula_user)
+        
+    }
+
+}
+
+pelicula_user.addEventListener("change", mostrar_Pelicula)
+
+function mostrar_Pelicula(){
+    const pelicula_user = document.getElementById("pelicula_user")
+    const span_pelicula = document.getElementById("span_pelicula")
+    const img_pelicula = document.getElementById("img_pelicula")
+    const pelicula_seleccionada = pelicula_user.options[pelicula_user.selectedIndex]
+   
+    span_pelicula.innerHTML = pelicula_seleccionada.value
+
+    if(pelicula_user.value == "X-men"){
+        img_pelicula.innerHTML = '<img src="assets/xmen-5.jpg" class="img_pelicula">'
+
+    }else if(pelicula_user.value == "Gran Torino"){
+        img_pelicula.innerHTML = '<img src="assets/grantorino.jpg" class="img_pelicula">'
+
+    }else if(pelicula_user.value == "Los 8 odiosos"){
+        img_pelicula.innerHTML = '<img src="assets/Los_odiosos_ocho-327104058-large.jpg" class="img_pelicula">'
+
+    }else if(pelicula_user.value = "Elige tu pelicula preferida"){
+        span_pelicula.innerHTML = ""
+        img_pelicula.innerHTML = ""
+    }
+
+
+}
